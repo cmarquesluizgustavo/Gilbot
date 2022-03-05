e5256e36a71c000340c46ffe0db1b9a8c111ffc0
Gustavo Miguel de Oliveira Vieira <gustavo@bgcbrasil.com.br>
2022-03-05 10:08:18 -0300

    docs: documenting wiki_requests

--- utils/wiki_requests.py
+++ utils/wiki_requests.py
@@ -9,7 +9,7 @@ wikipediaPageUrl   = "https://pt.wikipedia.org/wiki/{}"
 def filterKey(page):
     return page["key"]
 
-
+# Recebe o termo a ser buscado e retorna a chave a ser acessada na wikipedia
 def getPageKey(topic):
     topic     = quote_plus(topic)
 
@@ -23,10 +23,12 @@ def getPageKey(topic):
 
     raise Exception('Not found topic "{}"'.format(topic))
 
+# Recebe a chave e retorna o conteúdo existente na página solicitada
 def getPageHtml(key):
     html = urlopen(wikipediaPageUrl.format(key))
     return BeautifulSoup(html, 'html.parser')
 
+# Recebe o conteúdo da página e retorna apenas o primeiro parágrafo
 def getFirstParagraph(bs):
     div = bs.find('div', id="mw-content-text").div
     return div.p.text
